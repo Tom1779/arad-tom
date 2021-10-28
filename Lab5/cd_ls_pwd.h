7cf0dc9838d026925e0af7e8fe20dab39e60abab
Tom Arad <tom.arad@wsu.edu>
2021-10-27 19:37:54 -0700

    Finished ls and cd commands

---
+++ Lab5/cd_ls_pwd.h
@@ -0,0 +1,37 @@
+#ifndef CD_LS_PWD_H
+#define CD_LS_PWD_H
+
+#include <pwd.h>
+#include <grp.h>
+#include <sys/stat.h>
+#include <ext2fs/ext2fs.h>
+
+#include "type.h"
+
+extern MINODE minode[NMINODE];
+extern MINODE *root;
+extern PROC proc[NPROC], *running;
+
+extern char gpath[128];
+extern char *name[64];
+extern int n;
+
+extern int fd, dev;
+extern int nblocks, ninodes, bmap, imap, iblk;
+
+extern char line[128], cmd[32], pathname[128];
+
+/* inode: i_mode */
+#define EXT2_S_IFMT 0xF000  /* format mask  */
+#define EXT2_S_IFDIR 0x4000 /* directory */
+
+int cd();
+int ls_file(MINODE *mip, char *name);
+int ls_dir(MINODE *mip);
+int ls(char *pathname);
+int rpwd(MINODE *wd);
+char *pwd(MINODE *wd);
+
+
+
+#endif
